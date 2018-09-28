8d173fcb9847e417a022961187468b619ef137a1
Chase Fahy <charlesfahy17@augustana.edu>
2018-09-28 13:58:24 -0500

    Connor fixed autotracking compiler errors

--- ProjectFrigateBird/src/datamodel/AnimalTrack.java
+++ ProjectFrigateBird/src/datamodel/AnimalTrack.java
@@ -4,24 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AnimalTrack {
-	public static final String UNNAMED_ID = "<<unassigned>>"; 
-	private String animalID = UNNAMED_ID;
+	private String animalID;
 	
 	private List<TimePoint> positions;
 	
-	public AnimalTrack() {
+	public AnimalTrack(String id) {
+		this.animalID = id;
 		positions = new ArrayList<TimePoint>();
 	}
 	
-	public boolean hasIDAssigned() {
-		return !animalID.equals(UNNAMED_ID);
+	public void add(TimePoint pt) {
+		positions.add(pt);
 	}
 	
-	public List<TimePoint> getPositions() {
-		return positions;
+	public TimePoint getTimePointAtIndex(int index) {
+		return positions.get(index);
+	}
+
+	/**
+	 * Returns the TimePoint at the specified time, or null
+	 * @param frameNum
+	 * @return
+	 */
+	
+	public TimePoint getTimePointAtTime(int frameNum) {
+		//TODO: This method's implementation is inefficient [linear search is O(N)]
+		//      Replace this with binary search (O(log n)] or use a Map for fast access
+		for (TimePoint pt : positions) {
+			if (pt.getFrameNum() == frameNum) {
+				return pt;
+			}
+		}
+		return null;
+	}
+	
+	public TimePoint getFinalTimePoint() {
+		return positions.get(positions.size()-1);
 	}
 	
 	public String toString() {
-		return "AnimalTrack[id="+ animalID + ",len=" + positions.size()+"]"; 
+		int startFrame = positions.get(0).getFrameNum();
+		int endFrame = getFinalTimePoint().getFrameNum();
+		return "AnimalTrack[id="+ animalID + ",numPts=" + positions.size()+" start=" + startFrame + " end=" + endFrame +"]"; 
 	}
 }
--- ProjectFrigateBird/src/datamodel/Video.java
+++ ProjectFrigateBird/src/datamodel/Video.java
@@ -11,6 +11,7 @@ public class Video {
 	
 	private String filePath;
 	private VideoCapture vidCap;
+	private int emptyFrameNum;
 	private int startFrameNum;
 	private int endFrameNum;
 	
@@ -24,11 +25,22 @@ public class Video {
 		this.vidCap = new VideoCapture(filePath);
 		if (!vidCap.isOpened()) {
 			throw new FileNotFoundException("Unable to open video file: " + filePath);
-		}		
+		}
+		//fill in some reasonable default/starting values for several fields
+		this.emptyFrameNum = 0;
+		this.startFrameNum = 0;
+		this.endFrameNum = this.getTotalNumFrames()-1;
+		
+		int frameWidth = (int)vidCap.get(Videoio.CAP_PROP_FRAME_WIDTH);
+		int frameHeight = (int)vidCap.get(Videoio.CAP_PROP_FRAME_HEIGHT);
+		this.arenaBounds = new Rectangle(0,0,frameWidth,frameHeight);
 	}
 	
 	public void setCurrentFrameNum(int seekFrame) {
 		vidCap.set(Videoio.CV_CAP_PROP_POS_FRAMES, (double) seekFrame);
+	}
+	public int getCurrentFrameNum() {
+		return (int) vidCap.get(Videoio.CV_CAP_PROP_POS_FRAMES);
 	}
 	
 	public Mat readFrame() {
@@ -40,6 +52,9 @@ public class Video {
 	public String getFilePath() {
 		return this.filePath;
 	}
+	/** 
+	 * @return frames per second
+	 */
 	public double getFrameRate() {
 		return vidCap.get(Videoio.CAP_PROP_FPS);
 	}
@@ -47,7 +62,14 @@ public class Video {
 		return (int) vidCap.get(Videoio.CAP_PROP_FRAME_COUNT);
 	}
 
-	
+	public int getEmptyFrameNum() {
+		return emptyFrameNum;
+	}
+
+	public void setEmptyFrameNum(int emptyFrameNum) {
+		this.emptyFrameNum = emptyFrameNum;
+	}
+		
 	public int getStartFrameNum() {
 		return startFrameNum;
 	}
@@ -80,6 +102,10 @@ public class Video {
 		this.yPixelsPerCm = yPixelsPerCm;
 	}
 
+	public double getAvgPixelsPerCm() {
+		return (xPixelsPerCm + yPixelsPerCm)/2;
+	}
+
 	public Rectangle getArenaBounds() {
 		return arenaBounds;
 	}
@@ -88,5 +114,12 @@ public class Video {
 		this.arenaBounds = arenaBounds;
 	}
 	
+	public double convertFrameNumsToSeconds(int numFrames) {
+		return numFrames / getFrameRate();
+	}
+
+	public int convertSecondsToFrameNums(double numSecs) {
+		return (int) Math.round(numSecs * getFrameRate());
+	}
 
 }
