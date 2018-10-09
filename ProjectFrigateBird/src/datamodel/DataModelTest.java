92d3c25678dc6d3b18ae60f71fc57e74283c2329
Connor McGing <connormcging17@augustana.edu>
2018-10-09 15:50:12 -0500

    Added JUnitTest file

---
+++ ProjectFrigateBird/src/datamodel/DataModelTest.java
@@ -0,0 +1,15 @@
+package datamodel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DataModelTest {
+
+	@Test
+	//Test with auto-tracking so we do not need to run program every time
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
